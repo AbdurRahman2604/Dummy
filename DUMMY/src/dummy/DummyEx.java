15c5dd300584318e137ebd91ed3ab981b0381931
monap <monap@DESKTOP-O8K8LF0>
2023-03-01 21:21:03 +0530

    first commit

---
+++ DUMMY/src/dummy/DummyEx.java
@@ -0,0 +1,18 @@
+package dummy;
+
+public class DummyEx {
+
+		public static class SimpleClass {
+		  static void method(){
+			System.out.println ("hi all");
+			System.out.println("welcome");
+		  }
+			public static void main(String[] args) {
+				method();
+				method();
+		 }
+		 }
+
+	}
+
+
